f40c5dcfb0ff88a48d2a7dad573a2cef53d86602
Francisco Barreneche <franbarreneche@gmail.com>
2020-12-18 18:48:58 -0300

    actualizacion seeder Artista

--- database/seeders/ArtistaSeeder.php
+++ database/seeders/ArtistaSeeder.php
@@ -17,10 +17,13 @@ class ArtistaSeeder extends Seeder
     {
         //$api_key = "e93600d650e78eb84e718e468e63756f";
         //$guzzle->setSslVerification(false);
-        $response = Http::get('https://api.themoviedb.org/3/person/popular?api_key=e93600d650e78eb84e718e468e63756f&language=es-ES&page=1')['results'];        
-        foreach($response as $artista) {
-            $nombre = $artista['name'];
-            Artista::create(["nombre" => $nombre]);
+        for($i=1;$i<10;$i++) {
+            $response = Http::get('https://api.themoviedb.org/3/person/popular?api_key=e93600d650e78eb84e718e468e63756f&language=es-ES&page='.$i)['results'];        
+            foreach($response as $artista) {
+                $nombre = $artista['name'];
+                Artista::create(["nombre" => $nombre]);
+            }
         }
+        
     }
 }

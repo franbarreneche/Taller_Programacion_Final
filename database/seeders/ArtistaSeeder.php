a548030cee56e79b473dd8f29f7e4192f2b53ce5
Francisco Barreneche <franbarreneche@gmail.com>
2020-12-18 18:37:56 -0300

    Seeder de tabla Artistas

---
+++ database/seeders/ArtistaSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Artista;
+use Illuminate\Support\Facades\Http;
+
+class ArtistaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //$api_key = "e93600d650e78eb84e718e468e63756f";
+        //$guzzle->setSslVerification(false);
+        $response = Http::get('https://api.themoviedb.org/3/person/popular?api_key=e93600d650e78eb84e718e468e63756f&language=es-ES&page=1')['results'];        
+        foreach($response as $artista) {
+            $nombre = $artista['name'];
+            Artista::create(["nombre" => $nombre]);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {        
         $this->call([
             GeneroSeeder::class,            
+            ArtistaSeeder::class,
         ]);
     }
 }

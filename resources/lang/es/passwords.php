d8fead4fb7716566de8ebef01d3e51754893b69f
Francisco Barreneche <franbarreneche@gmail.com>
2020-12-19 21:54:44 -0300

    agregada traduccion al español

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Su contraseña ha sido reiniciada!',
+    'sent' => 'Le hemos enviado un email con el link para el reinicio de la contraseña!',
+    'throttled' => 'Por favor, aguarde unos instantes antes de volver a intentar.',
+    'token' => 'Este token para reiniciar la contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de email.",
+
+];

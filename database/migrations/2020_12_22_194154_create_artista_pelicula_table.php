b1d2b09f65b0861c16fffd5df6956199562eb771
Francisco Barreneche <franbarreneche@gmail.com>
2020-12-22 16:45:32 -0300

    migration para crear tabla artista_pelicula

--- database/migrations/2020_12_22_194154_create_artista_pelicula_table.php
+++ database/migrations/2020_12_22_194154_create_artista_pelicula_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGeneroPeliculaTable extends Migration
+class CreateArtistaPeliculaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateGeneroPeliculaTable extends Migration
      */
     public function up()
     {
-        Schema::create('genero_pelicula', function (Blueprint $table) {
+        Schema::create('artista_pelicula', function (Blueprint $table) {
             $table->id();
             $table->timestamps();            
             $table->foreignId("pelicula_id")->constrained();
-            $table->foreignId("genero_id")->constrained();
+            $table->foreignId("artista_id")->constrained();
         });
     }
 
@@ -28,6 +28,6 @@ class CreateGeneroPeliculaTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('genero_pelicula');
+        Schema::dropIfExists('artista_pelicula');
     }
 }

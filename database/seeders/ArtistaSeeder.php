e987efbf9cd08bf5bd8f4eb8a798f143232b7bb4
Francisco Barreneche <franbarreneche@gmail.com>
2020-12-23 16:44:14 -0300

    agregada migration para genero y foto de artista

--- database/seeders/ArtistaSeeder.php
+++ database/seeders/ArtistaSeeder.php
@@ -21,7 +21,13 @@ class ArtistaSeeder extends Seeder
             $response = Http::get('https://api.themoviedb.org/3/person/popular?api_key=e93600d650e78eb84e718e468e63756f&language=es-ES&page='.$i)['results'];        
             foreach($response as $artista) {
                 $nombre = $artista['name'];
-                Artista::create(["nombre" => $nombre]);
+                $genero = $artista['gender']; //1 es mujer y 2 es hombre
+                $foto = $artista['profile_path'];
+                Artista::create([
+                    "nombre" => $nombre,
+                    "genero" => $genero,
+                    "foto" => "https://image.tmdb.org/t/p/w600_and_h900_bestv2".$foto
+                    ]);
             }
         }
         
